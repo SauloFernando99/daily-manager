9182b877304350dab8eb3142ce5c599a8e9066b2
SauloFernando99 <saulofernando114@gmail.com>
2024-11-20 09:37:34 -0300

    style: optimize imports

--- app/src/main/java/saulo/fernando/daily_manager/manager/notes/AddNoteScreen.kt
+++ app/src/main/java/saulo/fernando/daily_manager/manager/notes/AddNoteScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
@@ -22,8 +21,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
-import saulo.fernando.daily_manager.account.AuthRepository
 import saulo.fernando.daily_manager.composables.MyTopBar
 import java.util.Date
 
